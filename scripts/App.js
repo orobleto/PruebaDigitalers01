f3304d86a2e10e7e3781cfa009cda4d095b53a17
Octavio Robleto <octavio.robleto@gmail.com>
2021-07-05 20:14:09 -0300

    Revert "Se agregan los estilos al pie de pagina"
    This reverts commit 7f84c084c9c5ba888df143b00ee8aa274f1fabb0.
    This reverts commit e83f055c3f0f7caac49a6c39c523c2c2042fe70d.

--- scripts/App.js
+++ scripts/App.js
@@ -1,10 +1,5 @@
 console.log('Inicio del Programa');
 
 console.log('Intermedio');
-const nombres = ['Octavio', 'Mariana', 'Analia','Nahuel'];
-
-for (const nombre of nombres) {
-    console.log(nombre);
-}
 
 console.log('Fin del Programa');
