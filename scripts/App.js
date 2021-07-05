7f84c084c9c5ba888df143b00ee8aa274f1fabb0
Octavio Robleto <octavio.robleto@gmail.com>
2021-07-05 20:09:43 -0300

    Se agrega el arreglo de nombres

--- scripts/App.js
+++ scripts/App.js
@@ -1,5 +1,10 @@
 console.log('Inicio del Programa');
 
 console.log('Intermedio');
+const nombres = ['Octavio', 'Mariana', 'Analia','Nahuel'];
+
+for (const nombre of nombres) {
+    console.log(nombre);
+}
 
 console.log('Fin del Programa');
